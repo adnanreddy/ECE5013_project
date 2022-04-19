5b96a2d2ff429e3f61c6e41ef889b05379289631
adnanreddy <77280424+adnanreddy@users.noreply.github.com>
2022-04-19 01:12:43 -0400

    script for graph of each CPI, adjusted main script accordingly

---
+++ ECE5013_project_6.m
@@ -0,0 +1,29 @@
+%% 6.
+
+CPI = ceil(2*25*tand(10)*fp/v/64);
+initial_target = zeros(1,CPI);
+
+for i = 1:CPI
+    ECE5013_project;
+    
+    figure(i);
+    
+    subplot(2,2,1)
+    imagesc(taugrid,1:Np,abs(matcharray1));
+    xlabel('Range (m)'); ylabel('Pulse No');
+
+    subplot(2,2,2)
+    imagesc(taugrid,nugrid*lambda*fp/2, abs(rangedoppler1))
+    xlabel('Range (m)'); ylabel('Velocity (m/sec)');
+
+    subplot(2,2,3)
+    imagesc(taugrid,1:Np,abs(matcharray2));
+    xlabel('Range (m)'); ylabel('Pulse No');
+
+    subplot(2,2,4)
+    imagesc(taugrid,nugrid*lambda*fp/2, abs(rangedoppler2))
+    xlabel('Range (m)'); ylabel('Velocity (m/sec)');
+    
+    filename = strcat('ECE_5013_6_',num2str(i),'.jpg');
+    saveas(gcf,filename)
+end
